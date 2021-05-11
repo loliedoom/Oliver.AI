b5915c6ed7373a36fa2187474ed3b1e72b42df03
Anderson Chan <>
2021-05-11 06:08:48 +0000

    v003 - New read me, o-imagine, bug fixes, debug command, etc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Oliver.AI
-This is my first real Python project. Oliver.AI is an open-source Discord Bot that aims to respond to humans naturally. This is still a work in progress project.
+Oliver.AI is a Discord Bot that aims to respond to humans naturally. This is still a work in progress project, and much more needs to be done. You are free to use snippets of the code for your own reference or projects. You may not copy the entire project and claim it as your own work. A public release of this bot will be released to Discord in the near future.
--- main.py
+++ main.py
@@ -27,12 +27,29 @@ bot = ChatBot(
     ]
 )
 
+#2nd Bot
+bot2 = ChatBot('Olivia')
+bot2 = ChatBot(
+    'Olivia',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3.2'
+)
+bot2 = ChatBot(
+    "Olivia",
+    logic_adapters=[
+        "chatterbot.logic.BestMatch",
+    ]
+)
+
 #Training Data
 from chatterbot.trainers import ListTrainer
 
 bot.set_trainer(ListTrainer)
+bot2.set_trainer(ListTrainer)
 
 #Important Stuff Below
+
+Version = "v0.0.03b"
 
 sad_words = ["Fortnite", "Simp", "Sadness"]
 
@@ -69,7 +86,7 @@ def delete_encouragement(index):
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-    await client.change_presence(activity=discord.Game('v0.0.2 | Need help? Try: o-help'))
+    await client.change_presence(activity=discord.Game('v0.0.3b | Need help? Try: o-help'))
 
     @client.event
     async def on_message(message):
@@ -99,6 +116,62 @@ async def on_ready():
                 break
             else:
                 await message.channel.send(botfeedback)
+
+        if message.content.lower().startswith('o-imagine'):
+          channel = message.channel
+          BotUser = message.author
+          await channel.send("Give me something to imagine, and I'll give you an image.")
+          def check(m):
+              return m.author == BotUser
+          responsei = await client.wait_for("message",check=check)
+          import requests
+          r = requests.post(
+            "https://api.deepai.org/api/text2img",
+            data={
+              'text': str(responsei.content),
+            },
+            headers={'api-key': 'f07cc451-41d9-4cd4-9ce7-630b38d2fa7c'}
+          )
+          imagineoutput = str(r.json())
+          import re
+          imagineoutput2 = (re.search("(?P<url>https?://[^\s]+)", imagineoutput).group("url"))
+          imagineoutput3 = imagineoutput2[:-2]
+          await channel.send(imagineoutput3)
+
+        if msg.startswith("oimagine/"):
+          channel = message.channel
+          BotUser = message.author
+          convo_messageimagine = msg.split("oimagine/",1)[1]
+          responsei = convo_messageimagine
+          import requests
+          r = requests.post(
+            "https://api.deepai.org/api/text2img",
+            data={
+              'text': str(responsei),
+            },
+            headers={'api-key': 'f07cc451-41d9-4cd4-9ce7-630b38d2fa7c'}
+          )
+          imagineoutput = str(r.json())
+          import re
+          imagineoutput2 = (re.search("(?P<url>https?://[^\s]+)", imagineoutput).group("url"))
+          imagineoutput3 = imagineoutput2[:-2]
+          await channel.send("Here you go! " + str(BotUser) + " | " + str(imagineoutput3))
+
+        if message.content.lower().startswith('o-support'):
+          channel = message.channel
+          BotUser4 = message.author
+          await channel.send("Conversation locked with " + str(BotUser4) + ". Type o-quit to exit.")
+          while True:
+            def check(m):
+                return m.author == BotUser4 or m.content.lower().startswith('o-quit')
+            responsex = await client.wait_for("message",check=check)
+            botresponsex = responsex.content
+            botfeedbackx = bot2.get_response(botresponsex)
+            if responsex.content.startswith("o-quit"):
+                await channel.send("Conversation ended with " + str(BotUser4))
+                break
+            else:
+                await message.channel.send(botfeedbackx)
 
         if message.content.lower().startswith('o-train'):
           channel2 = message.channel
@@ -149,10 +222,19 @@ async def on_ready():
             if int(responsex3.content) <= 10:
               for x in range(0, int(responsex3.content)):
                 await message.channel.send(responsex2.content)
+                
+                responsex = await client.wait_for("message",check=check)
+                if responsex.content.startswith("o-abort"):
+                  await channel.send("Nuke process aborted.")
+                  break
 
         if message.content.lower().startswith('o-inspire'):
           quote = get_quote()
           await message.channel.send(quote)
+          
+        if message.content.lower().startswith('o-debug'):
+          BotUser = message.author
+          await message.channel.send("Bot Latency: " + str(client.latency) + " | Bot Version: " + (Version) + " | Guild: " + (BotUser.guild.name) + " | Message Author: " + str(BotUser))
 
         if db["responding"]:
           options = starter_encouragements
@@ -238,20 +320,17 @@ async def on_ready():
         #Credits
         if message.content.lower().startswith("o-credits"):
             embedVar = discord.Embed(
-                title="Oliver.AI v0.0.2 | Credits",
+                title="Oliver.AI v0.0.3 [Running on Genesis Code] | Credits",
                 description="The people who made Oliver.AI possible. Proudly written in Python 3. Oliver.AI is open source, check out our GitHub!",
                 color=0x83FFFF)
             embedVar.add_field(name="Development",
-                              value="Anderson (Loliedoom#2006)",
-                              inline=False)
-            embedVar.add_field(name="Artistic Design",
-                              value="Sir Lemon",
+                              value="Anderson (Loliedoom#5314)",
                               inline=False)
             embedVar.add_field(name="Inspiration",
                               value="My dearest friends.",
                               inline=False)
-            embedVar.add_field(name="APIs",
-                              value="Chatterbot, Discord.py, ZenQuotes.io",
+            embedVar.add_field(name="APIs and Libraries Used",
+                              value="Chatterbot, Discord.py, ZenQuotes.io, DeepAI Text To Image by Scott Ellison Reed",
                               inline=False)
             embedVar.add_field(name="GitHub",
                               value="https://github.com/loliedoom/Oliver.AI",
@@ -264,7 +343,7 @@ async def on_ready():
         #Request Help
         if message.content.lower().startswith("o-help"):
             embedVar2 = discord.Embed(
-              title = "Oliver.AI v0.0.2 | Help",
+              title = "Oliver.AI v0.0.3 [Running on Genesis Code] | Help",
               description = "Need help using Oliver.AI? Don't worry, we got you.", color=0x83FFFF)
             embedVar2.add_field(name="Credits and Settings",
                               value="Use [o-help] to open this panel. Use [o-credits] to get insight on the creators of Oliver.AI",
@@ -272,11 +351,20 @@ async def on_ready():
             embedVar2.add_field(name="Conversation [AI-Assisted]",
                               value="Use [o/] followed by your prompt to make a one-time request to Oliver's chat engine. Use [o-convo] to lock into a conversation. Use [o-train] to be tailor Oliver to certain responses. Don't worry, it will guide you through it.",
                               inline=False)
+            embedVar2.add_field(name="Emotional Support [AI-Assisted]",
+                              value="Use [o-support] to switch to Oliver's emotional support database. This command works similarily to [o-convo], but doesn't use the general response engine.",
+                              inline=False)
+            embedVar2.add_field(name="Imagine [AI-Assisted]",
+                              value="Use [o-imagine] to generate an image out of text. You can also do [oimagine/] following by the prompt to make a request in one line. For information on the API used, use [o-credits].",
+                              inline=False)
             embedVar2.add_field(name="Mindfulless",
                               value="Use [o-inspire] to get an inspring quote. Use [o-breathe] to start a breathing exercise.",
                               inline=False)
             embedVar2.add_field(name="Fun Commands",
                               value="Use [o-nuke] to flood a chat with your desired text. A password is required for values over 10.",
+                              inline=False)
+            embedVar2.add_field(name="Utility Commands",
+                              value="Use [o-debug] to get details such as latency, guild name, author name, and version number. ",
                               inline=False)
             embedVar2.add_field(name="[Deprecated] Pre-set messages.",
                               value="Use [o-list] to get a list of messages encoded. Use [o-new] to add a new message. Use [o-del #] to delete a message. Use [o-responding true/false] to toggle responding. Say any of these 3 words: Fortnite, Simp, Sadness, to trigger the response. *This feature is deprecated and is no longer supported, this is here for reference. Although, it still might work.*",
